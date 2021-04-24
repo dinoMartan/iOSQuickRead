4ce5c3a2c03ec897a4bb9f55711824b8420f0069
DinoMartan <dino.martan@gmail.com>
2021-04-24 15:09:19 +0200

    Adding iOS project

---
+++ QuickRead/QuickRead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickRead
+//
+//  Created by Dino Martan on 24/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
